0534619dc40f422c79258560d026855ebd3d59d5
Blake Pell <blakepell@hotmail.com>
2021-03-19 13:35:47 -0400

    Script engine stuff.

--- src/Avalon.Common/Scripting/IScriptEngine.cs
+++ src/Avalon.Common/Scripting/IScriptEngine.cs
@@ -17,10 +17,31 @@ namespace Avalon.Common.Scripting
     /// </summary>
     public interface IScriptEngine
     {
+        /// <summary>
+        /// An implementation of C# commands that can be exposed to the scripting engine through
+        /// the means that scripting provides for CLR object access.
+        /// </summary>
+        IScriptCommands ScriptCommands { get; set; }
+
+        /// <summary>
+        /// A reference to the Interpreter that a script may interact with.
+        /// </summary>
         IInterpreter Interpreter { get; set; }
 
+        /// <summary>
+        /// Executes code synchronously and returns <see cref="T"/> or null based.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="code"></param>
+        /// <returns></returns>
         T Execute<T>(string code);
 
+        /// <summary>
+        /// Executes code asynchronously and return <see cref="T"/> or null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="code"></param>
+        /// <returns></returns>
         Task<T> ExecuteAsync<T>(string code);
     }
 }
--- src/Avalon.Common/Scripting/NLuaEngine.cs
+++ src/Avalon.Common/Scripting/NLuaEngine.cs
@@ -7,46 +7,60 @@
  * @license           : MIT
  */
 
+
+using Argus.Memory;
 using Avalon.Common.Interfaces;
 using System;
-using System.IO;
-using System.Threading;
 using System.Threading.Tasks;
-using Argus.Memory;
-using Avalon.Common.Models;
 
 namespace Avalon.Common.Scripting
 {
+    /// <summary>
+    /// A Lua script engine provided through NLua.
+    /// </summary>
     public class NLuaEngine : IScriptEngine
     {
+        /// <summary>
+        /// A memory pool of idle Lua objects that can be re-used.
+        /// </summary>
         internal static ObjectPool<NLua.Lua> LuaMemoryPool { get; set; }
 
-        private IScriptCommands _cmds;
+        /// <inheritdoc cref="ScriptCommands"/>
+        public IScriptCommands ScriptCommands { get; set; }
 
+        /// <inheritdoc cref="Interpreter"/>
         public IInterpreter Interpreter { get; set; }
 
-        public NLuaEngine(IInterpreter interp, IScriptCommands cmds)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="interp"></param>
+        /// <param name="scriptCommands"></param>
+        public NLuaEngine(IInterpreter interp, IScriptCommands scriptCommands)
         {
             this.Interpreter = interp;
-            _cmds = cmds;
+            this.ScriptCommands = scriptCommands;
 
             LuaMemoryPool = new ObjectPool<NLua.Lua>
             {
                 InitAction = l =>
                 {
                     // Initiate any setup actions needed for this Lua script.
-                    l["lua"] = _cmds;
+                    l["lua"] = this.ScriptCommands;
                 },
                 ReturnAction = l =>
                 {
                     if (l.IsExecuting)
                     {
-                        throw new Exception("Lua object was returned to the pool that was still executing.");
+                        // By throwing an exception here the Lua object will not be returned to the pool and
+                        // will instead be disposed of.
+                        throw new Exception("Attempt to return a Lua object to the pool that was still executing failed.");
                     }
                 }
             };
         }
 
+        /// <inheritdoc cref="Execute{T}"/>
         public T Execute<T>(string code)
         {
             // Gets a new or used but ready instance of the a Lua object to use.
@@ -56,12 +70,20 @@ namespace Avalon.Common.Scripting
             // is returned to the pool.
             object[] ret;
 
-            ret = lua.DoString(code);
-            LuaMemoryPool.Return(lua);
+            try
+            {
+                ret = lua.DoString(code);
+                LuaMemoryPool.Return(lua);
+            }
+            catch
+            {
+                LuaMemoryPool.Return(lua);
+                throw;
+            }
 
             // If a result was returned cast it to T and return it, if not, return the default
             // which will be null for reference types.
-            if (ret.Length > 0)
+            if (ret != null && ret.Length > 0)
             {
                 return (T)ret[0];
             }
@@ -69,6 +91,7 @@ namespace Avalon.Common.Scripting
             return default(T);
         }
 
+        /// <inheritdoc cref="ExecuteAsync{T}"/>
         public Task<T> ExecuteAsync<T>(string code)
         {
             return Task.Run(() => this.Execute<T>(code));
